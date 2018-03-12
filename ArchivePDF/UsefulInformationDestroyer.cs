f2686b6285fc9e9fad4f3b5d027287abc164491a
KC Juntunen <juntunenkc@lan.amstore.com>
2018-03-12 11:24:36 -0400

    Add loop, wrap in try/catch.

--- ArchivePDF/UsefulInformationDestroyer.cs
+++ ArchivePDF/UsefulInformationDestroyer.cs
@@ -23,8 +23,10 @@ namespace ArchivePDF.csproj {
 					fs.Close();
 				}
 			} catch (IOException ioe) {
-				using (ErrMsg em = new ErrMsg(ioe)) {
-					em.ShowDialog();
+				System.Windows.Forms.MessageBox.Show(ioe.Message, @"Couldn't create redacted version.");
+			} catch (System.Exception e) {
+				using (ErrMsg er_ = new ErrMsg(e)) {
+					er_.ShowDialog();
 				}
 			}
 		}
@@ -32,39 +34,42 @@ namespace ArchivePDF.csproj {
 		private byte[] _RedactUsefulDataFromDocument() {
 			using (Document document = new Document()) {
 				using (MemoryStream memoryStream = new MemoryStream()) {
-					PdfCopy copy = new PdfCopy(document, memoryStream);
-					document.Open();
-					using (PdfReader pdfReader = new PdfReader(source)) {
-						for (int i = 1; i < pdfReader.NumberOfPages + 1; i++) {
-							PdfImportedPage importedPage = copy.GetImportedPage(pdfReader, i);
+					try {
+						PdfCopy copy = new PdfCopy(document, memoryStream);
+						document.Open();
+						using (PdfReader pdfReader = new PdfReader(source)) {
+							for (int i = 1; i < pdfReader.NumberOfPages + 1; i++) {
+								PdfImportedPage importedPage = copy.GetImportedPage(pdfReader, i);
 								PdfCopy.PageStamp pageStamp = copy.CreatePageStamp(importedPage);
 								PdfContentByte contentByte = pageStamp.GetOverContent();
 
-							if (i == 1) {
-								//Rectangle sourceRevRectangle = new Rectangle(21, 123, 44, 133);
-								//sourceRevRectangle.BackgroundColor = BaseColor.BLACK;
-								//Rectangle sourceECRRectangle = new Rectangle(46, 123, 71, 133);
-								//sourceECRRectangle.BackgroundColor = BaseColor.BLACK;
-								//Rectangle sourceDescrRectangle = new Rectangle(73, 123, 900, 133);
-								//sourceDescrRectangle.BackgroundColor = BaseColor.BLACK;
+								if (i == 1) {
+									float y_offset = 137F;
+									for (int j = 0; j < 5; j++, y_offset -= 13.5F) {
+										Rectangle sourceRevRectangle = new Rectangle(21, y_offset, 44, y_offset + 10);
+										sourceRevRectangle.BackgroundColor = BaseColor.WHITE;
+										Rectangle sourceECRRectangle = new Rectangle(46, y_offset, 71, y_offset + 10);
+										sourceECRRectangle.BackgroundColor = BaseColor.WHITE;
+										Rectangle sourceDescrRectangle = new Rectangle(73, y_offset, 988, y_offset + 10);
+										sourceDescrRectangle.BackgroundColor = BaseColor.WHITE;
 
-								Rectangle sourceRevRectangle = new Rectangle(21, 137, 44, 147);
-								sourceRevRectangle.BackgroundColor = BaseColor.BLACK;
-								Rectangle sourceECRRectangle = new Rectangle(46, 137, 71, 147);
-								sourceECRRectangle.BackgroundColor = BaseColor.BLACK;
-								Rectangle sourceDescrRectangle = new Rectangle(73, 137, 900, 147);
-								sourceDescrRectangle.BackgroundColor = BaseColor.BLACK;
+										contentByte.Rectangle(sourceRevRectangle);
+										contentByte.Rectangle(sourceECRRectangle);
+										contentByte.Rectangle(sourceDescrRectangle);
 
-								contentByte.Rectangle(sourceRevRectangle);
-								contentByte.Rectangle(sourceECRRectangle);
-								contentByte.Rectangle(sourceDescrRectangle);
-								pageStamp.AlterContents();
+										pageStamp.AlterContents();
+
+									}
+								}
+								copy.AddPage(importedPage);
 							}
-							copy.AddPage(importedPage);
 						}
+					} catch (System.Exception e) {
+						throw e;
+					} finally {
 						document.Close();
-						return memoryStream.GetBuffer();
 					}
+					return memoryStream.GetBuffer();
 				}
 			}
 		}
